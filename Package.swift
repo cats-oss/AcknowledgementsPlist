fe1607fa7fa6a389ce5b1aa57307ff72ff0d9ad0
Ryo Aoyama <r.fe51028.r@gmail.com>
2019-04-03 15:55:14 +0900

    Fix package config

--- Package.swift
+++ Package.swift
@@ -5,19 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "AcknowledgementsPlist",
+    products: [
+        .executable(name: "acknowledgementsplist", targets: ["acknowledgementsplist"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/kylef/Commander.git",
-                 from: "0.8.0")
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0")
     ],
     targets: [
         .target(
-            name: "AcknowledgementsPlist",
-            dependencies: ["Commander", "AcknowledgementsPlistCore"]),
+            name: "acknowledgementsplist",
+            dependencies: ["Commander", "AcknowledgementsPlistCore"]
+        ),
         .target(
             name: "AcknowledgementsPlistCore",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "AcknowledgementsPlistTests",
-            dependencies: ["AcknowledgementsPlist"]),
+            dependencies: ["acknowledgementsplist"]
+        ),
     ]
 )
